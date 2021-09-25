3fe011d64b0908b20866a2f9d181f54b733a1296
Koolkan <75653679+Koolkan0709@users.noreply.github.com>
2021-09-25 10:09:32 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -12,12 +12,20 @@ class Block{
       this.image = loadImage("block.png");
     }
     display(){
-      console.log(this.body.speed);
-      var pos= this.body.position;
-      rectMode(CENTER);
-      rect(pos.x,pos.y,this.width, this.height);
-      if (this.body.speed > 3){
-        World.remove(world, this.body);
+      //console.log(this.body.speed);
+      //var pos= this.body.position;
+      //rectMode(CENTER);
+      //rect(pos.x,pos.y,this.width, this.height);
+      if (this.body.speed < 3){
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y, 50, 50)
+        pop();
+      }
+      else{
+         World.remove(world, this.body);
         push();
         this.Visibility = this.Visibility - 5;
         tint(255, this.Visiblilty);
